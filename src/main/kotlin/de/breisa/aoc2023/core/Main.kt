1351ca5c129d06bc21f8665b65da278d5cceee78
breisa <breisa@posteo.me>
2023-12-06 15:11:15 +0100

    added solution for day 6

--- src/main/kotlin/de/breisa/aoc2023/core/Main.kt
+++ src/main/kotlin/de/breisa/aoc2023/core/Main.kt
@@ -3,6 +3,6 @@ package de.breisa.aoc2023.core
 import de.breisa.aoc2023.days.*
 
 fun main(args: Array<String>) {
-    val days = listOf(Day01(), Day02(), Day03(), Day04(), Day05())
+    val days = listOf(Day01(), Day02(), Day03(), Day04(), Day05(), Day06())
     days.last().solvePuzzles()
 }
---
+++ src/main/kotlin/de/breisa/aoc2023/days/Day06.kt
@@ -0,0 +1,39 @@
+package de.breisa.aoc2023.days
+
+import de.breisa.aoc2023.core.getResourceAsText
+
+/**
+ * https://adventofcode.com/2023/day/6
+ */
+class Day06: Day(
+    number = 6,
+    firstExample = """
+        Time:      7  15   30
+        Distance:  9  40  200
+    """.trimIndent(),
+    actualPuzzle = getResourceAsText("/day06/puzzle.txt")
+) {
+
+    override fun solveFirstPart(puzzle: String) = parseRaces(puzzle).map { countWinningRaces(it) }.reduce(Int::times)
+
+    override fun solveSecondPart(puzzle: String) = countWinningRaces(parseLongRace(puzzle))
+
+    private fun countWinningRaces(r: Race) = (0..r.time).count { holdTime -> distance(holdTime, r.time) > r.distance }
+
+    private fun distance(holdTime: Long, totalTime: Long) = (totalTime - holdTime) * holdTime
+
+    private fun parseRaces(puzzle: String): List<Race> =
+        puzzle.lines().let { (times, distances) ->
+            val timeValues = times.split(Regex(" +")).drop(1).map { it.toLong() }
+            val distanceValues = distances.split(Regex(" +")).drop(1).map { it.toLong() }
+            timeValues.zip(distanceValues).map { (time, distance) -> Race(time, distance) }
+        }
+
+    private fun parseLongRace(puzzle: String): Race =
+        puzzle.replace(" ", "").lines()
+            .map { it.split(":").last().toLong() }
+            .let { (time, distance) -> Race(time, distance) }
+
+    data class Race(val time: Long, val distance: Long)
+
+}
